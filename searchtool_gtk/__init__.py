99d927242f84890845516625b16250eaf6ffee84
Ianis Vasilev <ianis@ivasilev.net>
2025-07-15 23:38:53 +0300

    Remove redundant export blocks in init files

--- searchtool_gtk/__init__.py
+++ searchtool_gtk/__init__.py
@@ -2,12 +2,3 @@ from .exceptions import SearchToolError, SearchToolValidationError
 from .gui import SearchToolApp
 from .mode import SearchToolMode
 from .config import build_modes_from_file
-
-
-__all__ = [
-    'SearchToolApp',
-    'SearchToolError',
-    'SearchToolValidationError',
-    'SearchToolMode',
-    'build_modes_from_file'
-]
--- searchtool_gtk/modes/__init__.py
+++ searchtool_gtk/modes/__init__.py
@@ -2,11 +2,3 @@ from .bin import BinMode
 from .file import FileMode
 from .pipe import PipeMode
 from .cliphist import ClipHistMode
-
-
-__all__ = [
-    'BinMode',
-    'FileMode',
-    'PipeMode',
-    'ClipHistMode'
-]
