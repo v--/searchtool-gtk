31e8f4544b5cfea246eff0fb41aedfbfda02c966
Ianis Vasilev <ianis@ivasilev.net>
2026-07-09 15:57:48 +0300

    Mark modules as lazy in the main __init__.py

--- src/searchtool_gtk/__init__.py
+++ src/searchtool_gtk/__init__.py
@@ -1,3 +1,14 @@
+__lazy_modules__ = [
+    'searchtool_gtk.client',
+    'searchtool_gtk.collation',
+    'searchtool_gtk.config',
+    'searchtool_gtk.exceptions',
+    'searchtool_gtk.gui',
+    'searchtool_gtk.modes',
+    'searchtool_gtk.server',
+]
+
+
 from .client import activate, activate_cli, dmenu, dmenu_cli
 from .collation import PathCollator, SearchToolCollator, StringCollator
 from .config import load_modes_from_config_file
