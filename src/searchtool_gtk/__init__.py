ccc9f7eaa6470a72f37f471a141d92ee5a4d604c
Ianis Vasilev <ianis@ivasilev.net>
2026-07-09 14:57:22 +0300

    Initialize GTK in the GUI-specific code only

--- src/searchtool_gtk/__init__.py
+++ src/searchtool_gtk/__init__.py
@@ -1,10 +1,3 @@
-# ruff: file-ignore[non-empty-init-module, module-import-not-at-top-of-file]
-import gi
-
-
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
-
 from .client import basic_client, dmenu_client
 from .collation import PathCollator, SearchToolCollator, StringCollator
 from .config import load_modes_from_config_file
--- src/searchtool_gtk/gui/__init__.py
+++ src/searchtool_gtk/gui/__init__.py
@@ -1,4 +1,10 @@
-# ruff: file-ignore[non-empty-init-module]
+# ruff: file-ignore[non-empty-init-module, module-import-not-at-top-of-file]
+import gi
+
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+
 
 from .app import SearchToolApp
 from .styling import apply_styling
